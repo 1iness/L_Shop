cf32a15ce61262801b072eaf3703805d0d0d5fc7
am1xol <ruslannikitsin@gmail.com>
2026-03-22 21:36:52 +0300

    жсон с товарами, получение и добавление товаров

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import userRoutes from './routes/userRoutes.js';
+import productRoutes from './routes/productRoutes.js';
 import express from 'express';
 import type { Request, Response } from 'express'; 
 
@@ -7,6 +8,7 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
 
 app.get('/api/health', (req: Request, res: Response) => {
     res.status(200).json({ message: 'Server is running' });
